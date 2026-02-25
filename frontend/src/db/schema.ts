6e6a1214e38215b26fb922e4136ef929f122c7c5
Shamiun Shadid <shamiunshadid@gmail.com>
2026-02-25 12:12:07 +0600

    1. Added and modified users schema. 2. created sessuios schema.

--- frontend/src/db/schema.ts
+++ frontend/src/db/schema.ts
@@ -1,8 +1,22 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     fullName: varchar({length: 255}).notNull(),
     email: varchar({length: 255}).notNull().unique(),
-    password: varchar({length: 255}).notNull()
-})
+    password: varchar({length: 255}).notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
+    updatedAt: timestamp().defaultNow().notNull(),
+    deletedAt: timestamp(),
+});
+
+export const sessions = pgTable("sessions", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    userId: integer().notNull().references(()=> users.id, {onDelete: "cascade"}),
+    sessionToken: text(),
+    userAgent: text(),
+    ip: varchar({length: 45}).notNull(),
+    expiresAt: timestamp().defaultNow().notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
+    updatedAt: timestamp().defaultNow().notNull(),
+});
