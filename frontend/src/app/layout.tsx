a0ad34fd7795225f85c940b18113f92b2619fe8f
Shamiun Shadid <shamiunshadid@gmail.com>
2026-02-25 12:00:13 +0600

    Installed sonner component and added to the project through root layout.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Instrument_Serif, Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/Theme/theme-provider";
+import { Toaster } from "@/components/ui/sonner"
 
 const openSans = Open_Sans({
   variable: "--font-open-sans",
@@ -49,6 +50,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
         {children}
+        <Toaster position="top-center" richColors/>
         </ThemeProvider>
       </body>
     </html>
