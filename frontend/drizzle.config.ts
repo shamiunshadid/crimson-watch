8a956d77d75f30d8c9142332ab5145695736a5a7
Shamiun Shadid <shamiunshadid@gmail.com>
2026-03-05 21:49:02 +0600

    1. Installed uuid 2. added seeding script 3. modified the path of the drizzle output

--- frontend/drizzle.config.ts
+++ frontend/drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './src/db',
+  out: './src/db/drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
