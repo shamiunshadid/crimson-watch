e045b46fa996194c286126e42d33d026b5b23283
Shamiun Shadid <shamiunshadid@gmail.com>
2026-03-13 23:22:10 +0600

    removed some unnecessery files.

--- frontend/src/app/dashboard/sign-out.action.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { deleteSession } from "@/features/auth/server/use-cases/sessions";
-import { redirect } from "next/navigation";
-
-export async function signOut() {
-  await deleteSession();
-  redirect("/");
-};
